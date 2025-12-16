eb7c52b1866fc599278ed6fc8d596dc5884f0cf9
ahmad quraan <ahmadquraan@git.gmail.com>
2025-12-16 22:09:03 +0300

    Solving substr

--- 1) programming_fundamentals/Common-Core-Libft/libft.h
+++ 1) programming_fundamentals/Common-Core-Libft/libft.h
@@ -6,33 +6,35 @@
 /*   By: aqoraan <aqoraan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 22:05:33 by aqoraan           #+#    #+#             */
-/*   Updated: 2025/12/11 21:38:54 by aqoraan          ###   ########.fr       */
+/*   Updated: 2025/12/16 21:53:53 by aqoraan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-# define LIBFT_H
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
+#define LIBFT_H
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-typedef struct node
-{
-	void		*content;
-	struct node	*next_node;
-}				t_list;
-t_list			*ft_lstnew(void *content);
-int				ft_isalnum(int c);
-int				ft_isalpha(int c);
-int				ft_isascii(int c);
-int				ft_isdigit(int c);
-int				ft_isprint(int c);
-size_t			ft_strlen(const char *str);
-void			*ft_memmove(void *dest, const void *src, size_t len);
-void			*ft_memcpy(void *dest, const void *src, size_t len);
-t_list			*ft_lstnew(void *content);
-void			ft_bzero(void *arr, size_t size);
-int				ft_isalnum(int c);
-int				ft_isascii(int c);
+typedef struct node {
+  void *content;
+  struct node *next_node;
+} t_list;
+t_list *ft_lstnew(void *content);
+int ft_isalnum(int c);
+int ft_isalpha(int c);
+int ft_isascii(int c);
+int ft_isdigit(int c);
+int ft_isprint(int c);
+size_t ft_strlen(const char *str);
+void *ft_memmove(void *dest, const void *src, size_t len);
+void *ft_memcpy(void *dest, const void *src, size_t len);
+t_list *ft_lstnew(void *content);
+void ft_bzero(void *arr, size_t size);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+char *ft_strdup(const char *s);
 
 #endif
